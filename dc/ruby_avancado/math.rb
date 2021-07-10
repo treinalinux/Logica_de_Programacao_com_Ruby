06e22424bd234cd0a52e8164892fb83cd0ff614e
Alan da Silva Alves <linuxfama@gmail.com>
2021-07-10 19:15:36 -0300

    feat: working with math

---
+++ dc/ruby_avancado/math.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# square root of x
+raiz_quadrada = Math.sqrt(64)
+
+puts raiz_quadrada
+
+# logarithm
+logaritmo = Math.log10(10_000)
+puts logaritmo
+
+radian = 30 * (Math::PI / 180)
+
+cosine = Math.cos(radian)
+
+puts cosine
+
+puts Math::E
+puts Math::PI
